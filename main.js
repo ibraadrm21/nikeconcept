2aedfd8dc2f4e42624756a0f0d46f2f062d0f337
ibraadrm21 <121961716+ibraadrm21@users.noreply.github.com>
2025-01-23 12:22:40 +0100

    This is the 0.1v of the webpage!
    My first version, i'll keep this updated.

---
+++ main.js
@@ -0,0 +1,9 @@
+function buybtn(){
+    console.log("Buying");
+    location.href="airforce.html";
+}
+
+function changeImage(src) {
+    document.getElementById("current-image").src = src;
+  }
+  
