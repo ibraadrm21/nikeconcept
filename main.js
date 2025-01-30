da7a15e988b01fa41f536d59aedcd83689ff61c4
ibraadrm21 <121961716+ibraadrm21@users.noreply.github.com>
2025-01-30 18:36:44 +0100

    Delete main.js
    alredy in the /website folder

--- main.js
+++
@@ -1,9 +0,0 @@
-function buybtn(){
-    console.log("Buying");
-    location.href="airforce.html";
-}
-
-function changeImage(src) {
-    document.getElementById("current-image").src = src;
-  }
-  
